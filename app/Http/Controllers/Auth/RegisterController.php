c9cfd20a6726e3ddae9f97018f3c04cdfce2a469
Faschal <faschal21@gmail.com>
2020-12-01 13:38:49 +0700

    Add authenticated state

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,8 @@ class RegisterController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-
+        
+        auth()->attempt($request->only('email', 'password'));
         return redirect()->route('dashboard');
     }
 }
