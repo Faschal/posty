8f78f92278dd20deafcdbfccca147df45c1be193
Faschal <faschal21@gmail.com>
2020-12-03 10:07:24 +0700

    Add pagination to posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -29,6 +29,9 @@
                 <p class="mb-2">{{ $post->body }}</p>
               </div>
           @endforeach
+
+          {{ $posts->links() }}
+
         @else
           <p>There are no posts</p>    
         @endif
